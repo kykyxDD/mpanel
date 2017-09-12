8b1dac8db47c67d16b0db08544bb1ce7f80ae7a1
NastyaShcherbakova <kykyxDD@gmail.com>
2017-09-12 19:20:04 +0300

    del files

--- script_angular/scrollSvg.js
+++ script_angular/scrollSvg.js
@@ -12,28 +12,15 @@ mpanelApp.directive('scrollSvg', function() {
             h: elem_view.offsetHeight
         }
         var maxZoom = Math.max(size_svg.h/size_view.h,size_svg.w/size_view.w)
-        // var minZoom = Math.min(size_svg.h/size_view.h,size_svg.w/size_view.w)
-        // console.log(maxZoom, minZoom)
-        // console.log(size_view, size_svg)
-        /*$("svg").svgPanZoom({
-            // zoomFactor: 1,
-            maxZoom: Math.max(3, maxZoom),
-            animationTime: 0,
-            // limits:{
-            //     // x: 0,
-            //     // y: 0
-            // }
-        });*/
+
         $('svg').svgPan('viewport');
     }
     return {
         require: 'ngModel',
         restrict: 'A',
         link: function (scope, el, attr, ngModel) {
-
-
             scope.$watch(attr.ngModel, function(newValue){
-                // console.log(newValue)
+
                 if(newValue){
                     setTimeout(startScroll, 100)
                 }
